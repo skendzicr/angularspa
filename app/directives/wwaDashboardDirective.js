936d50b2569782e35673013e004c04e5c1644de7
RSkendzic <rskendzic@execom.eu>
2016-07-29 15:36:15 +0200

    added headers and titles

--- app/directives/wwaDashboardDirective.js
+++ app/directives/wwaDashboardDirective.js
@@ -6,6 +6,8 @@ angular.module("app").directive("wwaDashboard", function() {
     template: "<ps-dashboard></ps-dashboard>",
     link: function(scope) {
 
+      scope.title = 'My first Dashboard';
+
       scope.gridsterOpts = {
         columns: 12,
         margins: [20, 20],
@@ -17,11 +19,13 @@ angular.module("app").directive("wwaDashboard", function() {
 
 
       scope.widgets = [{
+        title: "First",
         sizeX: 3,
         sizeY: 3,
         row: 0,
         col: 0
       }, {
+        title:"Second",
         sizeX: 2,
         sizeY: 4,
         row: 0,
