fb7b7bbceb9a5305684ee46752d690acfc110d00
RSkendzic <rskendzic@execom.eu>
2016-08-01 09:21:30 +0200

    compiling widget content

---
+++ app/widgets/wwaEmployee/wwaEmployeeDirective.js
@@ -0,0 +1,11 @@
+"use strict";
+
+angular.module('app').directive('wwaEmployee',['dataService',function(dataService){
+return{
+templateUrl: 'app/widgets/wwaEmployee/wwaEmployeeTemplate.html',
+link: function(scope,el,attrs)
+{
+
+}
+};
+}]);
---
+++ app/widgets/wwaInventory/wwaInventoryDirective.js
@@ -0,0 +1,11 @@
+"use strict";
+
+angular.module('app').directive('wwaInventory',['dataService',function(dataService){
+return{
+templateUrl: 'app/widgets/wwaInventory/wwaInventoryTemplate.html',
+link: function(scope,el,attrs)
+{
+
+}
+};
+}]);
